c6e83d006d4a70753bb610142aa709f5674c28a8
khalifardy_miqdarsah <itdev.support22@tokiomarine-life.co.id>
2023-05-17 09:29:42 +0700

    berkas baru:     connect_wifi.py

---
+++ connect_wifi.py
@@ -0,0 +1,17 @@
+def connect():
+    import network
+
+    ssid = "Ali"
+    password = "20180427"
+
+    station = network.WLAN(network.STA_IF)
+    if not station.isconnected():
+        print("menghubungkan ke jaringan WiFI")
+    station.active(True)
+    station.connect(ssid, password)
+
+    while not station.isconnected():
+        pass
+
+    print("koneksi WIFI terhubung")
+    print(station.ifconfig())
